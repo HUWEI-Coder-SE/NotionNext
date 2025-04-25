e6f49167e61e71f73afb586316c86c639b459e52
HUWEI-Coder-SE <huwei2623@gmail.com>
2025-04-25 22:06:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,10 +51,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/HUWEI0721',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: 'http://hjw-vip.github.io',
+  HEO_INFO_CARD_TEXT3: '学术主页',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
